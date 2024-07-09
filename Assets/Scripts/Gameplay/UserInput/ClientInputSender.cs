5973f1bb42859215edc9d88658ec863eec653fee
scott johnson - mac <tahomasan@icloud.com>
2024-07-09 15:49:06 -0700

    minor changes to mouse controls

--- Assets/Scripts/Gameplay/UserInput/ClientInputSender.cs
+++ Assets/Scripts/Gameplay/UserInput/ClientInputSender.cs
@@ -522,7 +522,7 @@ namespace Unity.BossRoom.Gameplay.UserInput
 
                 if (Input.GetMouseButtonDown(0))
                 {
-                    // RequestAction(CharacterClass.Skill1.ActionID, SkillTriggerStyle.MouseClick);
+                    RequestAction(GameDataSource.Instance.GeneralTargetActionPrototype.ActionID, SkillTriggerStyle.MouseClick);
                 }
 
                 if (Input.GetMouseButtonDown(1))
